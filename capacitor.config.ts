0c70bfe26154e58bb3c7bc54ac811719dc30664d
tahawy111 <amertahawy111@gmail.com>
2023-03-12 00:16:05 +0200

    Fix Risponsive Problem

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "io.ionic.starter",
+  appId: "calendar.tahawy.com",
   appName: "بلوك النتيجة ببلاش",
   webDir: "build",
   bundledWebRuntime: false,
